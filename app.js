780b382bc76c144992617d37ec6878acb220bbdd
nareshbabuv <nareshbabuv@vmware.com>
2020-03-26 11:28:53 +0530

    Updated app.js with text to sppech and speech to text

--- app.js
+++ app.js
@@ -1,66 +1,80 @@
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var port = process.env.PORT || 8080;        // set our port
-var router = express.Router();              // get an instance of the express Router
+const logger = require('morgan');
+const textToSpeech = require('@google-cloud/text-to-speech');
+const speech = require('@google-cloud/speech');
+const bodyParser = require('body-parser');
+const express = require('express');
+const router = express.Router();
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ server });
+const util = require('util');
+const fs = require('fs');
+const path = require('path');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+module.exports = router;
+const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+app.set('port', PORT);
+app.set('env', NODE_ENV);
+
+app.use(logger('tiny'));
 app.use(bodyParser.json());
 
-async function main() {
-    // Imports the Google Cloud client library
-    const speech = require('@google-cloud/speech');
-    const fs = require('fs');
+app.use('/', router);
 
-    // Creates a client
-    const client = new speech.SpeechClient();
+const client = new textToSpeech.TextToSpeechClient();
+const clientsp = new speech.SpeechClient();
 
-    // The name of the audio file to transcribe
-    const fileName = './audio.raw';
+wss.on('connection',  function(WebSocket)  {
+    //connection is up, let's add a simple simple event
+    WebSocket.send('Hello, This message is from Server to Client. Connection Status : Connected!!');
+    WebSocket.on('message', async function(text) {
+        const request = {
+            input: {text: text},
+            // Select the language and SSML voice gender (optional)
+            voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+            // select the type of audio encoding
+            audioConfig: {audioEncoding: 'LINEAR16'},
+        };
 
-    // Reads a local audio file and converts it to base64
-    const file = fs.readFileSync(fileName);
-    const audioBytes = file.toString('base64');
+        const [response] = await client.synthesizeSpeech(request);
+        // Write the binary audio content to a local file
+        const writeFile = util.promisify(fs.writeFile);
+        await writeFile('output.mp3', response.audioContent, 'binary');
+        console.log('Audio content written to file: output.mp3');
+        console.log('App - Relative Path to the file is :', path.resolve('output.mp3'))
+        //const filePath = `${path.resolve('output.mp3')}`
+        const file = fs.readFileSync(`${path.resolve('output.mp3')}`);
+        const audioBytes = file.toString('base64');
 
-    // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-    const audio = {
-        content: audioBytes,
-    };
-    const config = {
-        encoding: 'LINEAR16',
-        sampleRateHertz: 16000,
-        languageCode: 'en-US',
-    };
-    const request = {
-        audio: audio,
-        config: config,
-    };
+        // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+        const audio = {
+            content: audioBytes,
+        };
+        const config = {
+            encoding: 'LINEAR16',
+            //sampleRateHertz: 16000,
+            languageCode: 'en-US',
+        };
+        const req = {
+            audio: audio,
+            config: config,
+        };
 
-    // Detects speech in the audio file
-    const [response] = await client.recognize(request);
-    const transcription = response.results
-        .map(result => result.alternatives[0].transcript)
-        .join('\n');
-    console.log(`Transcription: ${transcription}`);
+        // Detects speech in the audio file
+        const [resp] = await  clientsp.recognize(req);
+        const transcription = resp.results
+            .map(result => result.alternatives[0].transcript)
+            .join('\n');
+        console.log(`Transcription: ${transcription}`);
+        WebSocket.send(`${transcription}`)
+    })
 
-    return transcription
-}
+})
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', async function(req, res) {
-    //res.json({ message: 'hooray! welcome to our api!' });
-    let resp = await main().catch(console.error)
-    res.json({message: resp});
-
+server.listen(process.env.PORT || 8080, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
 });
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
--- client.js
+++ client.js
@@ -1,8 +1,12 @@
 const WebSocket = require('ws');
-const url = 'ws://localhost:8999'
+const url = 'ws://localhost:8080'
 const connection = new WebSocket(url)
+const util = require('util');
+
+
+
 connection.onopen = () => {
-    connection.send('hey')
+    connection.send('Good for you')
 }
 
 connection.onerror = error => {
@@ -10,8 +14,9 @@ connection.onerror = error => {
 }
 
 connection.onmessage = (message) => {
-    console.log("Received Message: ", message)
+    console.log("Received Message: ", message.data)
 }
+
 connection.close = () => {
     console.log("Disconnected")
 }
